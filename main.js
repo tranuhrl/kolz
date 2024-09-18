f112cb6ce222b12eae1cc8a2cdf6f5dd08271131
tranuhrl <116113037+tranuhrl@users.noreply.github.com>
2024-09-17 22:36:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function yt()
+{
+    window.open("https://www.youtube.com/@king_bobby_the_I")
+}
+
+function yt2()
+{
+    window.open("https://www.youtube.com/@skibidistouchings")
+}
+
+function github()
+{
+    window.open("https://github.com/tranuhrl")
+}
+
+function discord()
+{
+    window.open("discord://")
+}
